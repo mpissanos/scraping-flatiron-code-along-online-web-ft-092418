02d372c8a373d2ef31a8c608a6aa1724dcb3d61a
Manny Pissanos <mpissano@gmail.com>
2018-10-10 17:53:04 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,6 +17,10 @@ class Scraper
       course.description = post.css("p").text
     end
   end
+  
+  def get_courses
+    selg.get_page.css('.post')
+  end
     
     def print_courses
     self.make_courses
